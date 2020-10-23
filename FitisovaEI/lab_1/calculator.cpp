d122db10ce003297145f4703f1fb0177c99ec75e
kate-fitisova <kate-fitisova@users.noreply.github.com>
2020-10-23 14:16:16 +0300

    Create calculator.cpp

---
+++ FitisovaEI/lab_1/calculator.cpp
@@ -0,0 +1,57 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	float number1, number2;
+	char sign;
+	cout << "Enter two numbers and a sign"<< endl;
+	cin >> number1 >> number2 >> sign;
+	
+	while (true)
+	{
+		switch (sign)
+		{
+		case '+':
+			cout << number1 + number2 << endl;
+			cout << "Enter new values" << endl;
+			break;
+		case '-':
+			cout << number1 - number2<< endl;
+			cout << "Enter new values" << endl;
+			break;
+		case '*':
+			cout << number1 * number2<< endl;
+			cout << "Enter new values" << endl;
+			break;
+		case '/':
+			if (number2 == 0)
+			{
+				cout << "can not be divisible by 0" << endl;
+				cout << "Enter new values" << endl;
+			}
+			else
+			{
+				cout << number1 / number2<<endl;
+				cout << "Enter new values" << endl;
+			}
+			break;
+		default:
+			cout << "Enter a valid operation sign" << endl;
+			cout << "Enter new values" << endl;
+			break;
+		}
+		cout << "Exit the program?" << endl;
+		string answer;
+		cin >> answer;
+		if (answer == "yes") {
+			cout << "The programme is completed" << endl;
+			return 0;
+		}
+		else {
+			cin >> number1 >> number2 >> sign;
+		}
+	}
+	return 0;
+}
+
