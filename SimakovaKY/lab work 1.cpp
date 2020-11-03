5b5003da2b6597664181f53166bede100e306e62
KristinaSim <72194153+KristinaSim@users.noreply.github.com>
2020-11-03 16:05:43 +0300

    Update lab work 1.cpp

--- SimakovaKY/lab work 1.cpp
+++ SimakovaKY/lab work 1.cpp
@@ -3,34 +3,35 @@
 using namespace std;
 char ch = _getch();
 int main() {
+  cout << "Enter expression: first number +,-,*,/ second number  " << endl;
 	do {
 		
-		float  x;
-		float  y;
+		float  num1, num2, result;
 		char sign;
 
-		cin >> x >> sign >> y;
+		cin >> num1 >> sign >> num2;
 		switch (sign) {
 		case '+':
-			cout << "Result = " << x + y;
+			result = num1 + num2;
 			break;
 		case '-':
-			cout << "Result = " << x - y;
+			result =  num1 - num2;
 			break;
 		
 		case '/':
-			if (y == 0) {
+			if (num2 == 0) {
 				cout << " Can't be divided by 0 "; break;
 			}
 			 else {
-				cout << "Result = " << x / y;
+				result = num1 / num2;
 				break;}
 		case '*':
-			cout << "Result = " << x * y;
+			result = num1 * num2;
 			break;
 		default:
 			cout << " Sign unrecognised " << endl;
 		} 
-	} while (ch != 27);
+		cout << "Result= " << result;
+	} while (ch != 32);
 		return 0; 
 	} 
