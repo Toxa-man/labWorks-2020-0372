c4f7ca9c49cf522db1af6953a1539b9c4cb02abd
aboghanem <abo_ghanem.awsanPS@yahoo.com>
2020-11-27 03:37:22 +0300

    Create lab_4.cpp

---
+++ AboghanemAO/lab_4/lab_4.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <math.h> 
+
+
+double power(int num, int to_power) {
+	double answer = 1;
+	if (to_power == 0) { return 1; }
+	for (int i = 0; i < abs(to_power); i++) {
+		answer *= num;
+	}
+	if (to_power > 0) { return answer; }
+	else{ return 1/answer;}
+}
+
+
+ int factorial(int num) {
+	if (num == 0) { return 1; }
+	if (num < 0) { return -1; }
+	int answer = 1;
+	for (int i = 1; i <= num ; i++) {
+		answer *= i;
+	}
+	return answer;
+}
+
+
+double square_root(int num) {
+	if (num == 0) return 0;
+	if (num < 0) return -1;
+	double answer = 0 ;
+	double temp = 1;				
+
+	while (answer != temp) {
+		answer = temp;
+		temp = (answer + num / answer) / 2;
+	}
+	return answer;
+}
+
+
+const char* prime(int num) {
+	if (num <= 1) return "Not Prime";
+
+	for (int i = 2; i < num; i++) {
+		if (num % i == 0) { return "Not Prime"; }
+	}
+	return "Prime";
+}
+
+int main() {
+	int test1[5] = {1,0,25,9,4};
+	int test2[5] = {10,2,1,0,-2};
+	double test1_poowr_test2[5] = {1,0,25,1,0.0625};
+	int factorial_test2[5] = {3.6288E+6,2,1,1,-1};
+	double Square_root_test1[5] = {1,0,5,3,2};
+	const char* prime_test1[5] = { "Not Prime","Not Prime","Not Prime","Not Prime","Not Prime"};
+
+
+
+	std::cout << "------Exponentiation------\n" ;
+	for (int i = 0; i < 5 ;i++ )
+	{
+		std::cout << test1[i] << '^' << test2[i] <<" = " << test1_poowr_test2[i] <<" ";
+		if (power(test1[i], test2[i]) == test1_poowr_test2[i]) { std::cout << "True\n"; }
+		else { std::cout << "False\n"; }
+	}
+	
+
+	std::cout << "\n------Factorial------\n";
+	for (int i = 0; i < 5;i++)
+	{
+		std::cout << test2[i] << "! = " << factorial_test2[i] << " ";
+		if (factorial(test2[i]) == factorial_test2[i]) { std::cout << "True\n"; }
+		else { std::cout << "False\n"; }
+	}
+	
+
+	std::cout << "\n------Square root------\n";
+	for (int i = 0; i < 5;i++)
+	{
+		std::cout << test1[i] << "^ 0.5 = " << Square_root_test1[i] << " ";
+		if (square_root(test1[i]) == Square_root_test1[i]) { std::cout << "True\n"; }
+		else { std::cout << "False\n"; }
+	}
+
+	
+
+	std::cout << "\n------Prime------" << std::endl;
+	for (int i = 0; i < 5;i++)
+	{
+		std::cout << test1[i] << " is " << prime_test1[i] << " ";
+		if (prime(test1[i]) == prime_test1[i] ) { std::cout << "True\n"; }
+		else { std::cout << "False\n"; }
+	}
+	
+
+	system("pause");
+	return 0;
+}
