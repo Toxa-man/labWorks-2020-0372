fee4ecee1fc74d6a6d12fa0041ccb801f19f3b96
NickGAce <72158041+NickGAce@users.noreply.github.com>
2020-11-28 23:45:30 +0300

    Update lab_1.cpp

--- GorbachevNS/lab_1/lab_1.cpp
+++ GorbachevNS/lab_1/lab_1.cpp
@@ -2,33 +2,33 @@
 #include <windows.h>
 using namespace std;
 int main() {
-    float a, b;
-    char c;
+    float number_a, number_b;
+    char c_char;
     int i;
     while (true) {
         cout << "Enter first number: ";
-        cin >> a;
+        cin >> number_a;
         cout << "Enter second number: ";
-        cin >> b;
+        cin >> number_b;
         cout << "Enter operation: ";
-        cin >> c;
-     
-        switch (c) {
+        cin >> c_char;
+
+        switch (c_char) {
         case '+':
-            cout << a << "+" << b << "=" << a + b << '\n';
+            cout << number_a << "+" << number_b << "=" << number_a + number_b << '\n';
             break;
         case '-':
-            cout << a << "-" << b << "=" << a - b << '\n';
+            cout << number_a << "-" << number_b << "=" << number_a - number_b << '\n';
             break;
         case '*':
-            cout << a << "*" << b << "=" << a * b << '\n';
+            cout << number_a << "*" << number_b << "=" << number_a * number_b << '\n';
             break;
-        case '/': 
-            if ((b == 0) ) {
-                 cout << "Error\n";
-            break;
-        }
-            cout << a << "/" << b << "=" << a / b << '\n';
+        case '/':
+            if ((number_b == 0)) {
+                cout << "Error\n";
+                break;
+            }
+            cout << number_a << "/" << number_b << "=" << number_a / number_b << '\n';
             break;
         default:
             cout << "Error\n";
