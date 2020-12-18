03d4d69ea1758814605a070a07fcdaf8fde95d2c
Knyukua <74075239+Knyukua@users.noreply.github.com>
2020-12-18 22:05:40 +0300

    Delete gamepack.h

--- ToropovMA/source/gamepack.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include <iostream>
-
-void snakeGame(sf::RenderWindow& window);
-void tetrisGame(sf::RenderWindow& window);
-void tictactoesGame(sf::RenderWindow& window);
